d7121bbb5ef6d30b9becdff610b8dd68545d3326
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-24 00:22:40 -0800

    Deploy to heroku

--- server.js
+++ server.js
@@ -20,6 +20,32 @@ app.get("/api/hello", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+let responseObject = {};
+
+app.get("/api/timestamp:input", (req, res)=>{
+  let input = req.params.input;
+  if(input.includes('-')){
+    responseObject['unix'] = new Date(input).getTime;
+    responseObject['UTC'] = new Date(input).toUTCString;
+  } else {
+    input = parseInt(input);
+    
+  }
+});
+// A request to / api /: date ? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+
+// A request to / api /: date ? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00: 00: 00 GMT
+
+// A request to / api / 1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+// Your project can handle dates that can be successfully parsed by new Date(date_string)
+
+// If the input date string is invalid, the api returns an object having the structure { error: "Invalid Date" }
+
+// An empty date parameter should return the current time in a JSON object with a unix key
+
+// An empty date parameter should return the current time in a JSON object with a utc key
