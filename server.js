e941635a80f54e71cd0697e191a46f314c902e03
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-26 22:31:01 -0800

    Answer more of the prompt

--- server.js
+++ server.js
@@ -46,29 +46,14 @@ app.get('/api/:input', (req, res)=>{
   res.json(responseObject);
 });
 
-app.get('api/', (req, res)=>{
+app.get('/api', (req, res)=>{
   responseObject['unix'] = new Date().getTime();
+  responseObject['UTC'] = new Date().toUTCString();
 
   res.json(responseObject);
 })
-// A request to / api /: date ? with a valid date should return a JSON object with a unix key 
-  // that is a Unix timestamp of the input date in milliseconds
 
-
-// A request to / api /: date ? with a valid date should return a JSON object with a utc key 
-// that is a string of the input date in the format: Thu, 01 Jan 1970 00: 00: 00 GMT
-
-
-// A request to / api / 1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
-
+// A request to / api /: date ? with a valid date should return a JSON object with a utc key that 
+// is a string of the input date in the format: Thu, 01 Jan 1970 00: 00: 00 GMT
 
 // Your project can handle dates that can be successfully parsed by new Date(date_string)
-
-
-// If the input date string is invalid, the api returns an object having the structure { error: "Invalid Date" }
-
-
-// An empty date parameter should return the current time in a JSON object with a unix key
-
-
-// An empty date parameter should return the current time in a JSON object with a utc key
