61e32671fdfcc8c2e9ca3ae003351848fd05c775
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-28 17:17:59 -0800

    Rearrange order

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
 // init project
 var express = require('express');
 var app = express();
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
-});
-
-// your first API endpoint... 
-app.get('/api/hello', (req, res) => {
-  res.json({greeting: 'hello API'});
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 let responseObject = {};
@@ -53,4 +39,10 @@ app.get('/api', (req, res)=>{
   res.json(responseObject);
 });
 
-// Your project can handle dates that can be successfully parsed by new Date(date_string)
+var listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+// { "unix": 1451001600000, "utc": "Fri, 25 Dec 2015 00:00:00 GMT" }
